c401bce133c8af5f6b116c121c3cee38e2f8f148
MMarek <mildnermarek@gmail.com>
2020-01-05 19:42:28 +0100

    routing corects

--- oddam_w_dobre_rece/src/App.js
+++ oddam_w_dobre_rece/src/App.js
@@ -8,47 +8,22 @@ import {
 } from 'react-router-dom';
 
 
-import Home from "../components/Home/Home";
-import Login from "../components/Login/Login";
-import Register from "../components/Register/Register";
-import LogOut from "../components/LogOut/LogOut";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import LogOut from "./components/LogOut/LogOut";
 
 function App() {
   return (
       <HashRouter>
         <Switch>
           <Route exact path='/' component={Home}/>
-          <Route path='/logowanie' component={Login}/>
-          <Route path='/rejestracja' component={Register}/>
-          <Route path='/wylogowano' component={LogOut}/>
+          <Route path='/login' component={Login}/>
+          <Route path='/register' component={Register}/>
+          {/*<Route path='/logout' component={LogOut}/>*/}
         </Switch>
       </HashRouter>
   );
 }
-
-
-
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
