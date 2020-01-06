f718a0b7f5a976c13cfd4e067f1df44842573a5f
MMarek <mildnermarek@gmail.com>
2020-01-06 13:17:37 +0100

    4steps / button + title

---
+++ oddam_w_dobre_rece/src/components/Home/FourSteps/Button.js
@@ -0,0 +1,24 @@
+import React, {Component} from "react";
+import {Link} from "react-router-dom";
+
+class Button extends Component {
+    render() {
+        const buttonStyle = {
+            textDecoration: 'none',
+            paddingTop: '2.3125rem',
+            color: '#3C3C3C',
+        };
+
+        return (
+            <section className='buttonSection'>
+                <Link to='/Login' style={buttonStyle} className='hoverLink'>
+                    <p className='button'>
+                        Oddaj<br/>rzeczy
+                    </p>
+                </Link>
+            </section>
+        )
+    }
+}
+
+export default Button;
