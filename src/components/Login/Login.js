96a6b1051508ee9975d4759ed15db1a9cdebf103
MMarek <mildnermarek@gmail.com>
2020-01-08 11:18:48 +0100

    login layout

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,23 @@
 import React, {Component} from 'react';
-
+import Navigation from "../Home/Header/Navigation";
+import LoginRegister from "../Home/Header/LoginRegister";
+import decoration from "../../assets/Decoration.svg";
 
 
 class Login extends Component {
-
     render() {
         return (
             <>
+                <section className='logNav'>
+                    <LoginRegister/>
+                    <Navigation/>
+                </section>
+
+
+                <section className='loginForm'>
+                    <h3>Zaloguj się</h3>
+                    <img src={decoration} alt='decoration'/>
+                </section>
             </>
         )
     }
