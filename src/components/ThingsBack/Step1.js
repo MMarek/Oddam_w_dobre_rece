482e94487a61e86a0ce19376399936c4efefc529
MMarek <mildnermarek@gmail.com>
2020-01-20 19:19:10 +0100

    all steps

--- src/components/ThingsBack/Step1.js
+++ src/components/ThingsBack/Step1.js
@@ -1,60 +1,200 @@
-// import React, {Component} from 'react';
-// // import lastStep from "../../assets/Background-Form.jpg";
-// // import info from "";
+// // import React, {Component} from 'react';
+// // // import lastStep from "../../assets/Background-Form.jpg";
+// // // import info from "";
+// //
+// //
+// //
+// // class Step1 extends Component {
+// //
+// //     render() {
+// //
+// //         return (
+// //             <>
+// //
+// //
+// //
+// //
+// //                 {/*<div className='progressBar'>*/}
+// //                 {/*    <span className='bar activeBar'>1</span>*/}
+// //                 {/*    <span className='bar'>2</span>*/}
+// //                 {/*    <span className='bar'>3</span>*/}
+// //                 {/*    <span className='bar'>4</span>*/}
+// //                 {/*    <span className='bar'><img src={lastStep} alt={lastStep}/></span>*/}
+// //                 {/*</div>*/}
+// //                 {/**/}
+// //                 {/*<div className='titleStep'>*/}
+// //                 {/*    <span className='mobilechoose'>Zaznacz co chcesz oddać:</span>*/}
+// //                 {/*</div>*/}
+// //
+// //
+// //
+// //
+// //
+// //
+// //                 <div className='important'>
+// //                     <h2>Ważne!</h2>
+// //                     <p>Uzupełnij szczegóły dotyczące Twoich rzeczy. Dzięki temu będziemy wiedzieć komu najlepiej je
+// //                         przekazać.</p>
+// //                 </div>
+// //
+// //
+// //                 <div className='steps'>
+// //                     <span className='firstStep'>Krok 1/4</span>
+// //                     <span className='choose'>Zaznacz co chcesz oddać:</span>
+// //                     {this.props.radioError && <span className="errorStyle errorRadio">Zaznacz jedną z opcji!</span>}
+// //
+// //
+// //                     {/*<Step1Form handleRadioChange={this.props.handleRadioChange} radioValue={this.props.radioValue}/>*/}
+// //
+// //
+// //                     <div className='nextPrevButtons'>
+// //                         <span className='nextButton hoverLink'>Dalej</span>
+// //                     </div>
+// //                 </div>
+// //             </>
+// //         )
+// //     }
+// // }
+// //
+// //
+// // export default Step1;
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// import React, {Component} from 'react';
+// // import lastStep from "../../assets/Form-Hero-Image.jpg";
+// // import info from "../../assets/Background-Form.jpg";
+//
+//
+// class Step1Form extends Component {
+//     render() {
+//         return (
+//             <form className='radioForm'>
+//                 <div className='radioOptions'>
+//                     <input type='radio' id='clothesToUse' name="whatToGive"
+//                            value="ubrania, które nadają się do ponownego użycia"
+//                            onClick={this.props.handleRadioChange}
+//                            // checked={this.props.radioValue.indexOf("ubrania, które nadają się do ponownego użycia") > -1}
+//                     />
+//                     <label htmlFor='clothesToUse'>ubrania, które nadają się do ponownego użycia</label>
+//                 </div>
+//                 <div className='radioOptions'>
+//                     <input type='radio' id='clothesToThrow' name="whatToGive"
+//                            value="ubrania, do wyrzucenia"
+//                            onClick={this.props.handleRadioChange}
+//                            // checked={this.props.radioValue.indexOf("ubrania, do wyrzucenia") > -1}
+//                     />
+//                     <label htmlFor='clothesToThrow'>ubrania, do wyrzucenia</label>
+//                 </div>
+//                 <div className='radioOptions'>
+//                     <input type='radio' id='toys' name="whatToGive"
+//                            value="zabawki" onClick={this.props.handleRadioChange}
+//                            // checked={ this.props.radioValue.indexOf("zabawki") > -1}
+//                     />
+//                     <label htmlFor='toys'>zabawki</label>
+//                 </div>
+//                 <div className='radioOptions'>
+//                     <input type='radio' id='books' name="whatToGive"
+//                            value="książki" onClick={this.props.handleRadioChange}
+//                            // checked={ this.props.radioValue.indexOf("książki") > -1}
+//                     />
+//                     <label htmlFor='books'>książki</label>
+//                 </div>
+//                 <div className='radioOptions'>
+//                     <input type='radio' id='others' name="whatToGive"
+//                            value="inne" onClick={this.props.handleRadioChange}
+//                            // checked={ this.props.radioValue.indexOf("inne") > -1}
+//                     />
+//                     <label htmlFor='others'>inne</label>
+//                 </div>
+//             </form>
+//         );
+//     }
+// }
 //
 //
 // class Step1 extends Component {
 //
 //     render() {
 //
-//         return (
-//             <>
-//
-//
-//
-//
-//                 {/*<div className='progressBar'>*/}
-//                 {/*    <span className='bar activeBar'>1</span>*/}
-//                 {/*    <span className='bar'>2</span>*/}
-//                 {/*    <span className='bar'>3</span>*/}
-//                 {/*    <span className='bar'>4</span>*/}
-//                 {/*    <span className='bar'><img src={lastStep} alt={lastStep}/></span>*/}
-//                 {/*</div>*/}
-//                 {/**/}
-//                 {/*<div className='titleStep'>*/}
-//                 {/*    <span className='mobilechoose'>Zaznacz co chcesz oddać:</span>*/}
-//                 {/*</div>*/}
-//
-//
-//
-//
-//
-//
-//                 <div className='important'>
-//                     <h2>Ważne!</h2>
-//                     <p>Uzupełnij szczegóły dotyczące Twoich rzeczy. Dzięki temu będziemy wiedzieć komu najlepiej je
-//                         przekazać.</p>
-//                 </div>
-//
-//
-//                 <div className='steps'>
-//                     <span className='firstStep'>Krok 1/4</span>
-//                     <span className='choose'>Zaznacz co chcesz oddać:</span>
-//                     {this.props.radioError && <span className="errorStyle errorRadio">Zaznacz jedną z opcji!</span>}
-//
-//
-//                     {/*<Step1Form handleRadioChange={this.props.handleRadioChange} radioValue={this.props.radioValue}/>*/}
-//
-//
-//                     <div className='nextPrevButtons'>
-//                         <span className='nextButton hoverLink'>Dalej</span>
+//             return (
+//                 <>
+//                     <div className='progressBar'>
+//                         <span className='bar activeBar'>1</span>
+//                         <span className='bar'>2</span>
+//                         <span className='bar'>3</span>
+//                         <span className='bar'>4</span>
+//                         {/*<span className='bar'><img src={lastStep} alt={lastStep}/></span>*/}
 //                     </div>
-//                 </div>
-//             </>
-//         )
+//
+//
+//                     <div className='titleStep'>
+//                         <span className='mobilechoose'>Zaznacz co chcesz oddać:</span>
+//                     </div>
+//
+//
+//                     <div className='warningStep'>
+//                         {/*<img src={info} alt={info}/>*/}
+//                         <div className='warning'>
+//                             <h2>Ważne!</h2>
+//                             <p>Uzupełnij szczegóły dotyczące Twoich rzeczy. Dzięki temu będziemy wiedzieć komu najlepiej je przekazać.</p>
+//                         </div>
+//                     </div>
+//
+//
+//                     <div className='steps'>
+//                         {this.props.radioError && <span className="errorStyle errorRadio">Zaznacz jedną z opcji!</span>}
+//
+//
+//                         <Step1Form handleRadioChange={this.props.handleRadioChange} radioValue={this.props.radioValue}/>
+//
+//
+//                         <div className='nextPrevButtons'>
+//                             <span className='nextButton hoverLink' onClick={this.props.handleNext}>Dalej</span>
+//                         </div>
+//                     </div>
+//
+//                     <div className='warningStep'>
+//                         <h2>Ważne!</h2>
+//                         <p>Uzupełnij szczegóły dotyczące Twoich rzeczy. Dzięki temu będziemy wiedzieć komu najlepiej je przekazać.</p>
+//                     </div>
+//
+//
+//                     <div className='steps'>
+//                         <span className='stepNum'>Krok 1/4</span>
+//                         <span className='choose'>Zaznacz co chcesz oddać:</span>
+//                         {this.props.radioError && <span className="errorStyle errorRadio">Zaznacz jedną z opcji!</span>}
+//
+//
+//                         <Step1Form handleRadioChange={this.props.handleRadioChange} radioValue={this.props.radioValue}/>
+//
+//
+//                         <div className='nextPrevButtons'>
+//                             <span className='nextButton hoverLink' onClick={this.props.handleNext}>Dalej</span>
+//                         </div>
+//                     </div>
+//                 </>
+//             )
+//         }
 //     }
-// }
 //
 //
 // export default Step1;
@@ -72,16 +212,9 @@
 
 
 
-
-
-
-
-
-
-
 import React, {Component} from 'react';
-// import lastStep from "../../assets/Form-Hero-Image.jpg";
-// import info from "../../assets/Background-Form.jpg";
+import lastStep from './../../assets/Icon-2.svg'
+import info from './../../assets/Icon-3.svg'
 
 
 class Step1Form extends Component {
@@ -132,9 +265,34 @@ class Step1Form extends Component {
 
 
 class Step1 extends Component {
+    state = {
+        width: window.innerWidth,
+    };
+
+
+    componentWillMount() {
+        window.addEventListener('resize', this.handleWindowSizeChange);
+    }
+
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleWindowSizeChange);
+    }
+
+
+    handleWindowSizeChange = () => {
+        this.setState({ width: window.innerWidth });
+    };
+
 
     render() {
 
+
+        const { width } = this.state;
+        const isMobile = width <= 767;
+
+
+        if (isMobile) {
             return (
                 <>
                     <div className='progressBar'>
@@ -142,7 +300,7 @@ class Step1 extends Component {
                         <span className='bar'>2</span>
                         <span className='bar'>3</span>
                         <span className='bar'>4</span>
-                        {/*<span className='bar'><img src={lastStep} alt={lastStep}/></span>*/}
+                        <span className='bar'><img src={lastStep} alt={lastStep}/></span>
                     </div>
 
 
@@ -152,7 +310,7 @@ class Step1 extends Component {
 
 
                     <div className='warningStep'>
-                        {/*<img src={info} alt={info}/>*/}
+                        <img src={info} alt={info}/>
                         <div className='warning'>
                             <h2>Ważne!</h2>
                             <p>Uzupełnij szczegóły dotyczące Twoich rzeczy. Dzięki temu będziemy wiedzieć komu najlepiej je przekazać.</p>
@@ -171,7 +329,11 @@ class Step1 extends Component {
                             <span className='nextButton hoverLink' onClick={this.props.handleNext}>Dalej</span>
                         </div>
                     </div>
-
+                </>
+            )
+        } else {
+            return (
+                <>
                     <div className='warningStep'>
                         <h2>Ważne!</h2>
                         <p>Uzupełnij szczegóły dotyczące Twoich rzeczy. Dzięki temu będziemy wiedzieć komu najlepiej je przekazać.</p>
@@ -195,6 +357,7 @@ class Step1 extends Component {
             )
         }
     }
+}
 
 
 export default Step1;
--- src/components/ThingsBack/Step2.js
+++ src/components/ThingsBack/Step2.js
@@ -1,10 +1,111 @@
+// // import React, {Component} from 'react';
+// // import Select from 'react-styled-select';
+// //
+// //
+// // class Step2Form extends Component {
+// //     render() {
+// //
+// //
+// //         return (
+// //             <div className='selectForm'>
+// //                 <span>Liczba 60L worków:</span>
+// //             </div>
+// //         )
+// //     }
+// // }
+// //
+// //
+// // class Step2 extends Component {
+// //
+// //     render() {
+// //
+// //             return (
+// //                 <>
+// //                     <div className='progressBar'>
+// //                         <span className='bar doneBar'>1</span>
+// //                         <span className='bar activeBar'>2</span>
+// //                         <span className='bar'>3</span>
+// //                         <span className='bar'>4</span>
+// //                         {/*<span className='bar'><img src={lastStep} alt={lastStep}/></span>*/}
+// //                     </div>
+// //
+// //
+// //                     <div className='titleStep'>
+// //                         <span className='mobilechoose'>Podaj liczbę 60l worków, w które spakowałeś/aś rzeczy:</span>
+// //                     </div>
+// //
+// //
+// //                     <div className='warningStep'>
+// //                         {/*<img src={info} alt={info}/>*/}
+// //                         <div className='warning'>
+// //                             <h2>Ważne!</h2>
+// //                             <p>Wszystkie rzeczy do oddania zapakuj w 60l worki. Dokładną instrukcję jak poprawnie spakować rzeczy znajdziesz TUTAJ.</p>
+// //                         </div>
+// //                     </div>
+// //
+// //
+// //                     <div className='steps'>
+// //
+// //
+// //                         <Step2Form selectedOption={this.props.selectedOption} handleSelectChange={this.props.handleSelectChange}/>
+// //
+// //
+// //                         {this.props.errorSelect && <span className="errorStyle errorRadio">Wybierz jedną z opcji!</span>}
+// //                         <div className='nextPrevButtons'>
+// //                             <span className='prevButton hoverLink' onClick={this.props.handlePrevious}>Cofnij</span>
+// //                             <span className='nextButton hoverLink' onClick={this.props.handleNext}>Dalej</span>
+// //                         </div>
+// //                     </div>
+// //
+// //                     <div className='warningStep'>
+// //                         <h2>Ważne!</h2>
+// //                         <p>Wszystkie rzeczy do oddania zapakuj w 60l worki. Dokładną instrukcję jak poprawnie spakować rzeczy znajdziesz TUTAJ.</p>
+// //                     </div>
+// //
+// //
+// //                     <div className='steps'>
+// //                         <span className='stepNum'>Krok 2/4</span>
+// //                         <span className='choose'>Podaj liczbę 60l worków, w które spakowałeś/aś rzeczy:</span>
+// //
+// //
+// //                         <Step2Form selectedOption={this.props.selectedOption} handleSelectChange={this.props.handleSelectChange}/>
+// //
+// //
+// //                         {this.props.errorSelect && <span className="errorStyle errorRadio">Wybierz jedną z opcji!</span>}
+// //                         <div className='nextPrevButtons'>
+// //                             <span className='prevButton hoverLink' onClick={this.props.handlePrevious}>Cofnij</span>
+// //                             <span className='nextButton hoverLink' onClick={this.props.handleNext}>Dalej</span>
+// //                         </div>
+// //                     </div>
+// //                 </>
+// //             )
+// //         }
+// //     }
+// //
+// //
+// //
+// // export default Step2;
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 // import React, {Component} from 'react';
-// import Select from 'react-styled-select';
+// // import Select from 'react-styled-select';
+// // import lastStep from "../../assets/Form-Hero-Image.jpg";
+// // import info from "../../assets/Background-Form.jpg";
 //
 //
 // class Step2Form extends Component {
 //     render() {
-//
 //
 //         return (
 //             <div className='selectForm'>
@@ -83,9 +184,7 @@
 //     }
 //
 //
-//
 // export default Step2;
-
 
 
 
@@ -100,16 +199,38 @@
 
 import React, {Component} from 'react';
 // import Select from 'react-styled-select';
-// import lastStep from "../../assets/Form-Hero-Image.jpg";
-// import info from "../../assets/Background-Form.jpg";
+import lastStep from "../../assets/Icon-2.svg";
+import info from "../../assets/Icon-3.svg";
 
 
 class Step2Form extends Component {
     render() {
 
+
+        // const options = [
+        //     { label: "1", value: 1},
+        //     { label: "2", value: 2},
+        //     { label: "3", value: 3},
+        //     { label: "4", value: 4},
+        //     { label: "5", value: 5},
+        // ];
+
+
         return (
             <div className='selectForm'>
                 <span>Liczba 60L worków:</span>
+
+
+                {/*<Select*/}
+                {/*    value={this.props.selectedOption}*/}
+                {/*    onChange={this.props.handleSelectChange}*/}
+                {/*    getOptionLabel={option => option}*/}
+                {/*    getOptionValue={option => option}*/}
+                {/*    searchable={false}*/}
+                {/*    className="selectTheme"*/}
+                {/*    options={options}*/}
+                {/*    placeholder={"— wybierz —"}*/}
+                {/*/>*/}
             </div>
         )
     }
@@ -117,9 +238,36 @@ class Step2Form extends Component {
 
 
 class Step2 extends Component {
+    state = {
+        width: window.innerWidth,
+    };
+
+
+    componentWillMount() {
+        window.addEventListener('resize', this.handleWindowSizeChange);
+    }
+
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleWindowSizeChange);
+    }
+
+
+    handleWindowSizeChange = () => {
+        this.setState({ width: window.innerWidth });
+    };
+
 
     render() {
 
+
+
+
+        const { width } = this.state;
+        const isMobile = width <= 767;
+
+
+        if (isMobile) {
             return (
                 <>
                     <div className='progressBar'>
@@ -127,7 +275,7 @@ class Step2 extends Component {
                         <span className='bar activeBar'>2</span>
                         <span className='bar'>3</span>
                         <span className='bar'>4</span>
-                        {/*<span className='bar'><img src={lastStep} alt={lastStep}/></span>*/}
+                        <span className='bar'><img src={lastStep} alt={lastStep}/></span>
                     </div>
 
 
@@ -137,7 +285,7 @@ class Step2 extends Component {
 
 
                     <div className='warningStep'>
-                        {/*<img src={info} alt={info}/>*/}
+                        <img src={info} alt={info}/>
                         <div className='warning'>
                             <h2>Ważne!</h2>
                             <p>Wszystkie rzeczy do oddania zapakuj w 60l worki. Dokładną instrukcję jak poprawnie spakować rzeczy znajdziesz TUTAJ.</p>
@@ -157,7 +305,11 @@ class Step2 extends Component {
                             <span className='nextButton hoverLink' onClick={this.props.handleNext}>Dalej</span>
                         </div>
                     </div>
-
+                </>
+            )
+        } else {
+            return (
+                <>
                     <div className='warningStep'>
                         <h2>Ważne!</h2>
                         <p>Wszystkie rzeczy do oddania zapakuj w 60l worki. Dokładną instrukcję jak poprawnie spakować rzeczy znajdziesz TUTAJ.</p>
@@ -182,6 +334,7 @@ class Step2 extends Component {
             )
         }
     }
+}
 
 
 export default Step2;
